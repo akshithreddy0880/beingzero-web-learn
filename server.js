cfa8ad43ff7d323454391392cf778d9bde01340d
akshithreddy0880 <akshith0880@gmail.com>
2021-05-04 17:31:01 +0530

    resumeupdate

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static(__dirname + "/frontend"));
+
+app.get("/", function (req, res) {
+    res.send('Welcome to akshiths resume site');
+})
+app.get("/resume", function (req, res) {
+    let filepath = __dirname + "/frontend/html/resume.html";
+
+    res.sendFile(filepath);
+})
+app.get("/google", function (req, res) {
+    let filepath1 = __dirname + "/frontend/html/google.html"
+    res.sendFile(filepath1)
+})
+const PORT = process.env.PORT || 3000;
+//it is changed
+// Start the server
+app.listen(PORT, function () {
+    console.log("Server Starting running on http://localhost:" + PORT);
+})
